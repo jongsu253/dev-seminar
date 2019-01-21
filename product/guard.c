a73049877f3887e6ea82ad4a9725675539f6feca
jongsu253 <jongsu253@gmail.com>
2019-01-22 01:35:25 +0900

    Add debugger guard2

--- product/guard.c
+++ product/guard.c
@@ -2,11 +2,25 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/ptrace.h>
 
 void init_guard() {
-	if (ptrace(PTRACE_TRACEME) == -1) {
-		perror("Don't traceme!\n");
-		exit(-1);
+	int pid;
+
+	pid = fork();
+	if (pid == 0) {
+		int ppid = getppid();
+
+		if (ptrace(PTRACE_ATTACH, ppid, 0, 0) < 0) {
+			exit(-1);
+		}
+		ptrace(PTRACE_CONT);
+
+		sleep(5);
+	} else {
+		int status;
+		waitpid(pid, &status, 0);
 	}
+
 }
